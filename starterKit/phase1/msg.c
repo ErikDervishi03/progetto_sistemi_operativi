ffba7658c53df6f94bad0edd70c9bd5e44f42e6a
Fabzero22 <132613307+Fabzero22@users.noreply.github.com>
2023-12-15 14:39:58 +0100

    Update msg.c

--- starterKit/phase1/msg.c
+++ starterKit/phase1/msg.c
@@ -24,21 +24,53 @@ msg_t *allocMsg() {
         return tmp;
     }
 }
+#include "PandOS_types.h"
+#include "listx.h"
 
-void mkEmptyMessageQ(struct list_head *head) {
+// Inizializza una variabile come puntatore alla testa di una coda di messaggi vuota
+void mkEmptyProcQ(struct list_head *head) {
+    INIT_LIST_HEAD(head);
 }
 
+// Verifica se una coda di messaggi è vuota
 int emptyMessageQ(struct list_head *head) {
+    return list_empty(head);
 }
 
+// Inserisce un messaggio alla fine (coda) di una coda di messaggi
 void insertMessage(struct list_head *head, msg_t *m) {
+    list_add_tail(&m->m_list, head);
 }
 
+// Inserisce un messaggio all'inizio di una coda di messaggi
 void pushMessage(struct list_head *head, msg_t *m) {
+    list_add(&m->m_list, head);
 }
 
+// Rimuove e restituisce il primo messaggio dalla coda, eventualmente corrispondente al mittente
 msg_t *popMessage(struct list_head *head, pcb_t *p_ptr) {
+    if (list_empty(head)) {
+        return NULL; // Restituisce NULL se la coda dei messaggi è vuota
+    }
+
+    struct list_head *pos;
+    msg_t *msg;
+
+    // Itera attraverso la coda dei messaggi
+    list_for_each(pos, head) {
+        msg = container_of(pos, msg_t, m_list);
+
+        // Verifica se il mittente corrisponde a p_ptr (se p_ptr non è NULL)
+        if (p_ptr == NULL || msg->m_sender == p_ptr) {
+            list_del(&msg->m_list); // Rimuove il messaggio dalla coda
+            INIT_LIST_HEAD(&msg->m_list); // Inizializza il nodo della lista
+            return msg;
+        }
+    }
+
+    return NULL; // Restituisce NULL se nessun messaggio da p_ptr è stato trovato
 }
+
 
 msg_t *headMessage(struct list_head *head) {
 }
