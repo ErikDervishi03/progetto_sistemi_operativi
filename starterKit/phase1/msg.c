cf151f4a72aa976a05576900f878974b41392e44
sperti03 <72691236+sperti03@users.noreply.github.com>
2023-12-15 11:16:03 +0100

    Update msg.c
    inserite le prime 2 funzioni di msg (non so se funzionano non le ho testate)

--- starterKit/phase1/msg.c
+++ starterKit/phase1/msg.c
@@ -6,10 +6,23 @@ LIST_HEAD(msgFree_h);
 void initMsgs() {
 }
 
+//inserisci l'elemento puntato da m nella lista msgFree (lo metto in testa)
 void freeMsg(msg_t *m) {
+    list_del(&m->m_list);
+    list_add_tail(&m->m_list,&msgFree_h);
 }
-
+//ritorna NULL se la msgFree list è vuota, sennò rimuovi
+//un elemento dalla lista (assicurati di dare un valore a tutti 
+//i campi del messaggio e poi ritorna un puntatore all'elem rimosso)
 msg_t *allocMsg() {
+    if(msgFree_h==NULL)
+        return NULL;
+    else{
+        msg_t *tmp=container_of(msgFree_h.next,msg_t,*msg_PTR);
+        list_del(msgFree_h.next);
+        list_add(&tmp->m_list,&msgTable);
+        return tmp;
+    }
 }
 
 void mkEmptyMessageQ(struct list_head *head) {
